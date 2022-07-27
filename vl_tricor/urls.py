5291063da7346e0a3dfcf724947250ec01638f67
mod64bits <mod64bits@gmail.com>
2022-07-27 13:45:05 -0300

    Template

--- vl_tricor/urls.py
+++ vl_tricor/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from apps.home import urls as home_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include(home_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
